eadc8c22365903dfabac0e669e682d6805d176a2
Gustavo da Costa Fontana <162226403+gucfontana@users.noreply.github.com>
2024-05-08 15:41:32 -0300

    "ModuloCompartilhado"

---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; set; }
+
+        public abstract ArrayList Validar();
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,31 @@
+﻿namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    internal static class TelaPrincipal
+    {
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|       Clube da Leitura               |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Cadastro de Amigos");
+            Console.WriteLine("2 - Cadastro de Caixas");
+            Console.WriteLine("3 - Cadastro de Revistas");
+            Console.WriteLine("4 - Cadastro de Emprestimos");
+
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
+        }
+    }
+}
